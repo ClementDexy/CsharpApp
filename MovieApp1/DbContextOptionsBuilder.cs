a01ec846933b22db50a0114fea86b1434f926ab9
Dexy11 <dexypro7@gmail.com>
2019-02-24 10:52:05 +0200

    .Net console App added

---
+++ MovieApp1/DbContextOptionsBuilder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MovieApp1
+{
+    class DbContextOptionsBuilder
+    {
+        internal void UseSqlServer(string p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MovieApp1/DbSet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MovieApp1
+{
+    class DbSet<T>
+    {
+        internal void Add(Movietbl movietbl)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
